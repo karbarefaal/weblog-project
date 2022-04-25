621fb7ef288374260350f069e96b9e6f40ed6c3d
ahoora <sajjadia997@gmail.com>
2022-04-26 04:00:25 +0430

    remembering user for 1 day

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const path = require("path");
 
 const express = require("express");
+const mongoose = require('mongoose');
 const expressLayout = require("express-ejs-layouts");
 const passport = require('passport');
 const dotEnv = require("dotenv");
 const morgan = require("morgan");
 const flash = require('connect-flash');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 const connectDB = require("./config/db");
 
@@ -38,9 +40,10 @@ app.use(express.urlencoded({ extended: false }));
 //* Session
 app.use(session({
     secret: "secret",
-    cookie: { maxAge: 60000 },
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection })
     })
 );
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -14,10 +14,19 @@ exports.login = (req, res) => {
 
 exports.handleLogin = (req,res,next) => {
     passport.authenticate("local", {
-        successRedirect: "/dashboard",
+        //successRedirect: "/dashboard",
         failureRedirect: "/user/login",
         failureFlash: true // or you can send special text message 
     })(req,res,next);
+}
+
+exports.rememberMe = (req,res) => {
+    if(req.body.remember){
+        req.session.cookie.originalMaxAge = 24 * 60 * 60 * 1000; // 1 day or 24 hours
+    }else{
+        req.session.cookie.expire = null;
+    }
+    res.redirect("/dashboard");
 }
 
 exports.logout = (req,res) => {
