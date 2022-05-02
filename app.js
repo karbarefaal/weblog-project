2e8b8546811a58c6fedaa354cb800107a1cc5e83
ahoora <sajjadia997@gmail.com>
2022-05-02 21:52:26 +0430

    main page added but it needs category of languages

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const path = require("path");
 
-const debug = require('debug')("weblog-project");
-const bodyParser = require('body-parser');
+const debug = require("debug")("weblog-project");
+const fileUpload = require("express-fileupload");
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const expressLayout = require("express-ejs-layouts");
-const passport = require('passport');
+const passport = require("passport");
 const dotEnv = require("dotenv");
 const morgan = require("morgan");
-const flash = require('connect-flash');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+const flash = require("connect-flash");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 const connectDB = require("./config/db");
-const winston = require('./config/winston');
+const winston = require("./config/winston");
 
 //* Load Config
 dotEnv.config({ path: "./config/config.env" });
@@ -23,7 +23,7 @@ connectDB();
 debug("Connected To Database");
 
 //* Passport Configuration
-require('./config/passport');
+require("./config/passport");
 
 const app = express();
 
@@ -37,22 +37,24 @@ if (process.env.NODE_ENV === "development") {
 app.use(expressLayout);
 app.set("view engine", "ejs");
 app.set("layout", "./layouts/mainLayout");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", "views");
 
-//* Body Parser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+//* BodyPaser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//* File Upload Middleware
+app.use(fileUpload());
 
 //* Session
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    unset: "destroy",
-    store: new MongoStore({
-        mongooseConnection: mongoose.connection
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        unset: "destroy",
+        store: new MongoStore({ mongooseConnection: mongoose.connection }),
     })
-})
 );
 
 //* Passport
@@ -60,7 +62,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //* Flash
-app.use(flash()); // req.flash
+app.use(flash()); //req.flash
 
 //* Static Folder
 app.use(express.static(path.join(__dirname, "public")));
@@ -71,7 +73,7 @@ app.use("/users", require("./routes/users"));
 app.use("/dashboard", require("./routes/dashboard"));
 
 //* 404 Page
-app.use(require('./controllers/errorController').get404);
+app.use(require("./controllers/errorController").get404);
 
 const PORT = process.env.PORT || 3000;
 
--- models/Blog.js
+++ models/Blog.js
@@ -1,35 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const {schema} = require('./secure/postValidation');
+const { schema } = require("./secure/postValidation");
 
-const blogSchema = new mongoose.Schema({
+const blogSchmea = new mongoose.Schema({
     title: {
         type: String,
         required: true,
         trim: true,
         minlength: 5,
-        maxlength: 100
+        maxlength: 100,
     },
     body: {
         type: String,
-        required: true
-    }, status: {
+        required: true,
+    },
+    status: {
         type: String,
         default: "public",
-        enum: ["private","public"]
+        enum: ["private", "public"],
+    },
+    thumbnail: {
+        type: String,
+        required: true,
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+        ref: "User",
     },
-    createdAt:{
+    createdAt: {
         type: Date,
-        default: Date.now
-    }
+        default: Date.now,
+    },
 });
 
-blogSchema.statics.postValidation = function(body){
-    return schema.validate(body,{abortEarly: false});
+blogSchmea.statics.postValidation = function (body) {
+    return schema.validate(body, { abortEarly: false });
 };
 
-module.exports = mongoose.model("Blog",blogSchema);
+module.exports = mongoose.model("Blog", blogSchmea);
